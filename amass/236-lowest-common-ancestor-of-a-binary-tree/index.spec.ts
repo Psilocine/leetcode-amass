15009df13c762d1623c8451c1dc9f135f4821653
PsiloLau <psilolau@foxmail.com>
2022-05-18 20:34:46 +0800

    feat: create 236

---
+++ amass/236-lowest-common-ancestor-of-a-binary-tree/index.spec.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from "vitest";
+
+describe("二叉树的最近公共祖先", () => {});
+
+function testCase(fn: unknown) {
+  it.each([
+    // test cases
+  ])("示例%#", () => {
+    expect(true).toBe(true);
+  });
+}
